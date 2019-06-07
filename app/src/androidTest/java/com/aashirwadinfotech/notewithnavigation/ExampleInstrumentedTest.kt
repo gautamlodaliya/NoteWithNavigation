e6acc6ef088bf459571bfe315fe21011c0455c7b
gautamlodaliya <gblodaliya9@gmail.com>
2019-06-07 15:41:00 +0530

    first commit

---
+++ app/src/androidTest/java/com/aashirwadinfotech/notewithnavigation/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.aashirwadinfotech.notewithnavigation
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.aashirwadinfotech.notewithnavigation", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/aashirwadinfotech/notewithnavigation/ui/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.aashirwadinfotech.notewithnavigation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.aashirwadinfotech.notewithnavigation.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = Navigation.findNavController(this,R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(Navigation.findNavController(this,R.id.fragment),null)
+    }
+}
